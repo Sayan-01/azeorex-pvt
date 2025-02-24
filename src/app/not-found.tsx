4ba4cda8028f911c2e41b52809e25f547b46da1d
Sayan Das <sayandas61668@gmail.com>
2025-02-24 18:33:47 +0530

    19th production commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Custom404 = () => {
   return <div className="w-full h-screen flex items-center justify-center">
-    <Image src="/not_Found.jpg" width={400} height={300} alt="404" className="md:h-48 h-28"/>
+    <Image src="/not_Found.jpg" width={400} height={300} alt="404" className="md:h-48 md:w-[24rem] h-16 w-32"/>
   </div>;
 };
 
