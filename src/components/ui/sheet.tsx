d74307c15e52f5951931bc1ef86a0a34f04f7b53
Sayan Das <sayandas61668@gmail.com>
2025-02-24 13:33:42 +0530

    17th production commit

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -41,9 +41,13 @@ const sheetVariants = cva(
   }
 );
 
-interface SheetContentProps extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>, VariantProps<typeof sheetVariants> {}
+interface CustomSheetContentProps {
+  showX?: boolean;
+}
 
-const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Content>, SheetContentProps>(({ side = "right", className, children, ...props }, ref) => (
+interface SheetContentProps extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>, CustomSheetContentProps, VariantProps<typeof sheetVariants> {}
+
+const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Content>, SheetContentProps>(({ side = "right", showX, className, children, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
@@ -52,8 +56,11 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
       {...props}
     >
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+        {showX && (
+          <div className="flex items-center justify-center  bg-zinc-950 w-6 h-6 rounded-full md:hidden">
+            <X />
+          </div>
+        )}
       </SheetPrimitive.Close>
       {children}
     </SheetPrimitive.Content>
