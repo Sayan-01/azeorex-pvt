9ff012d2b90164b07a594a5ee4e23259a224da32
Sayan Das <sayandas61668@gmail.com>
2025-03-18 00:28:29 +0530

    try to fix viewport problem try-11

--- src/app/saas/layout.tsx
+++ src/app/saas/layout.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Sidebar from "./_components/sidebar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full flex min-h-screen bg-[#141414]">
+    <div className="w-full flex h-screen bg-[#141414] ">
       <Sidebar />
-      <main className="flex-1 h-full overflow-x-hidden md:ml-[240px]">{children}</main>
+      <main className="flex-1 h-full overflow-x-hidden box-1 md:ml-[240px]">{children}</main>
     </div>
   );
 };
--- src/app/saas/projects/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const layout = ({children}:{children:React.ReactNode}) => {
-  return <div className="min-h-screen">{children}</div>;
-};
-
-export default layout;
