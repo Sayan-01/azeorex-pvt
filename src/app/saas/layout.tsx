583ae5c352e2d836dbb323161446d1bb0d345167
Sayan Das <sayandas61668@gmail.com>
2025-03-17 23:13:32 +0530

    try to fix viewport problem try-9

--- src/app/saas/layout.tsx
+++ src/app/saas/layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full flex min-h-screen bg-[#141414]">
       <Sidebar />
-      <main className="flex-1 h-full overflow-y-auto box md:ml-[240px]">{children}</main>
+      <main className="flex-1 min-h-screen box md:ml-[240px]">{children}</main>
     </div>
   );
 };
