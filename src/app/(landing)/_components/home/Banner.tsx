88f0b758e8712ea9cf35f653885f1d57cba10781
Sayan Das <sayandas61668@gmail.com>
2025-11-10 19:42:17 +0530

    banner change

--- src/app/(landing)/_components/home/Banner.tsx
+++ src/app/(landing)/_components/home/Banner.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className=" -mt-[11.5rem] relative max-w-[100rem] mx-auto">
+    <div className=" -mt-[12rem] relative max-w-[100rem] mx-auto">
       <div className=" sm:w-full w-[200%] top-20 sm:top-0 relative -left-8 sm:left-0 ">
         <Image
           alt="banner"
-          src={"/banner-2.png"}
+          src={"/banner-3.png"}
           width={3500}
           height={2500}
           className=" w-full h-full"
